19bbb1714aa0ec5307cfaee0d830cdf9ff90927f
JMKobayashi <james.kobayashi@usp.br>
2021-08-24 22:18:19 -0300

    Iniciando estudos de Go

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque <= c.saldo && valorDoSaque > 0
+
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	} else {
+		return "Valor do deposito inválido", c.saldo
+	}
+
+}
+func main() {
+	contaDoJames := ContaCorrente{"James", 123, 123456, 153.54}
+
+	retorno, saldo := contaDoJames.Depositar(1000)
+	fmt.Println(retorno, saldo)
+}
